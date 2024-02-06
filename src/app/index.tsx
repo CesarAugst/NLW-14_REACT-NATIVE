d749085df9b86bcee53730c08e83f01f9d0524b7
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2024-02-06 08:43:31 -0300

    carrinho no header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/header";
 export default function Home(){
     return (
         <View className="flex-1 pt-8">
-            <Header title="Faça o seu pedido"/>
+            <Header title="Faça o seu pedido" cartQuantityItems={10}/>
         </View>
     )
 }

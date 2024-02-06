f27b05df74589e6e3bc219bcf273e50f9cf2aeaf
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2024-02-06 08:55:26 -0300

    categoryButton implementado

--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 - npm i tailwindcss@3.3.2 --save-dev
     - npx tailwindcss init
 - npx expo install expo-font @expo-google-fonts/inter
+- npm i clsx
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,17 @@
 import {View} from "react-native";
 import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
 
 export default function Home(){
     return (
         <View className="flex-1 pt-8">
             <Header title="Faça o seu pedido" cartQuantityItems={10}/>
+
+            <View className="flex-row gap-4">
+                <CategoryButton title="Lanche do dia" isSelected/>
+                <CategoryButton title="Lanche do dia"/>
+                <CategoryButton title="Lanche do dia"/>
+            </View>
         </View>
     )
 }

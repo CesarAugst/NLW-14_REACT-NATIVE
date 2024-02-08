da3bc90b213c039794cfc7596cc66560340216da
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2024-02-08 07:35:35 -0300

    ajustando passagem de parametros para sections list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { View, FlatList, SectionList, Text } from "react-native";
 import { useState, useRef } from "react";
 
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
@@ -15,7 +15,7 @@ export default function Home(){
 
     const [category, setCategory] = useState(CATEGORIES[0])
 
-    const sectionListRef = useRef<SectionList>(null);
+    const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
     const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0)
 
